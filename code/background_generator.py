db2073beef91e6099d145914d05ec10ca05985c4
Maxwell Joslyn <maxwelljoslyn@gmail.com>
2022-01-13 17:12:53 -0800

    add more information to "Background for..." output line

--- code/background_generator.py
+++ code/background_generator.py
@@ -304,7 +304,7 @@ def main(testing):
             dots = "." * (16 - len(name))
             f.write("{0}{1}{2}".format(name,dots,stat))
             f.write("\n")
-        f.write("Background for " + c.name + f" the {c.race} {c.pClass}:")
+        f.write("Background for " + c.name + f" the {c.sex} {c.race} {c.pClass}:")
         f.write("\n\n")
         f.write("Family:")
         f.write("\n")
