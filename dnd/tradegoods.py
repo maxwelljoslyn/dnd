22c64d951e3693ff387c396186705c9a31e32fc5
Maxwell Joslyn <maxwelljoslyn@gmail.com>
2022-03-16 22:42:54 -0700

    function to calculate animal food while being raised

--- dnd/tradegoods.py
+++ dnd/tradegoods.py
@@ -1718,6 +1718,23 @@ Recipe(
     vendor="tobacconist",
     description="powdered tobacco, unscented",
 )
+
+
+def fodder_while_growing(
+    start_age, end_age, start_weight, end_weight, daily_food_per_bodyweight
+):
+    """Return the total amount of fodder needed to raise an animal from `start_age` to `end_age`."""
+    months = (end_age - start_age).to(u.month)
+    growth = (end_weight - start_weight).to(u.lb)
+    result = D(0) * u.lb
+    # TODO clean this up so I can use Decimals instead of having to lossily convert to Ints and back
+    for i in range(1, int(months.magnitude) + 1):
+        i = D(i) * u.month
+        # age = start_age + i # used for debugging
+        weight = start_weight + i * (growth / months)
+        fodder = (daily_food_per_bodyweight * weight) * D(30)  # thirty days/month
+        result += fodder
+    return result
 donkey_sale_weight = D(650) * u.lb
 donkey_foal_age = D(1) * u.year
 donkey_foal_weight = donkey_sale_weight / D(2)
