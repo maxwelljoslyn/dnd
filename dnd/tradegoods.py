376ee1e2cd999d583acec94e7eb2052452d9f862
Maxwell Joslyn <maxwelljoslyn@gmail.com>
2022-04-23 14:49:45 -0700

    coat of plates

--- dnd/tradegoods.py
+++ dnd/tradegoods.py
@@ -3280,6 +3280,26 @@ make_armor(
     },
     "AC +2; stiffened cowhide armor with full sleeves, covering wearer to the waist",
 )
+
+
+bodyarmor_plating_thickness = D(1.5) * u.mm
+coatofplates_reinforced_area = D(0.8) * torso_area
+coatofplates_plate_weight = (
+    density["steel"] * coatofplates_reinforced_area * bodyarmor_plating_thickness
+).to(u.lb)
+coatofplates_weight = leatherarmor_weight + coatofplates_plate_weight
+
+make_armor(
+    "coat of plates",
+    coatofplates_weight,
+    {},
+    {
+        "boiled leather": leatherarmor_leather,
+        "woolen yarn": leatherarmor_string,
+        "steel": coatofplates_plate_weight,
+    },
+    "AC +3; as leather armor, but reinforced with metal plates between the leather layers",
+)
 ## a nitrate (niter is KNO3) + copper sulfate -> copper nitrate
 ## decomposition: copper nitrate Cu(NO3)2 + H2O -> copper oxide + 2 HNO3 (nitric acid)
 ## 2 KNO_3 + CuS + H_2O ⟶  CuO + 2 HNO_3 + S + 2 K
