48e05b0a5262ff1c8facc74edeb9c20c972ff279
Maxwell Joslyn <maxwelljoslyn@gmail.com>
2022-04-23 14:09:25 -0700

    add templates init

--- dnd/__web__/templates/__init__.py
+++ dnd/__web__/templates/__init__.py
@@ -1,7 +1,40 @@
 import re
+from pathlib import Path
+from web import tx
+from dnd import to_fewest_coins, to_copper_pieces
+from dnd import characters
+from characters import mod_to_text, final_abilities
 
-__all__ = ["titlecase"]
+__all__ = [
+    "tx",
+    "titlecase",
+    "Path",
+    "static_dir",
+    "to_fewest_coins",
+    "to_copper_pieces",
+    "mod_to_text",
+    "characters",
+    "final_abilities",
+    "ana",
+]
 
 
 def titlecase(s):
-    return re.sub(r"[A-Za-z]+('[A-Za-z]+)?", lambda mo: mo.group(0).capitalize(), s)
+    def helper(s):
+        # don't change all-caps items, which are probably abbreviations ... sigh, if only we had *structures* instead of strings... (text 'blah blah blah' (abbr 'CIA' :full 'Central Intelligence Agency') 'blah blah blah')
+        return s if s.isupper() else s.capitalize()
+
+    return re.sub(r"[A-Za-z]+('[A-Za-z]+)?", lambda mo: helper(mo.group(0)), s)
+
+
+def static_dir():
+    return Path(__file__).parent.parent / "static"
+
+
+def ana(word):
+    if not word:
+        raise ValueError()
+    elif word[0] in ("a", "e", "i", "o", "u"):
+        return "an"
+    else:
+        return "a"
