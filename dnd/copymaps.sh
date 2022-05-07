5ce2e4b16c1be372531330b1121c41029a5c5819
Maxwell Joslyn <maxwelljoslyn@gmail.com>
2022-05-07 09:44:19 -0700

    Add script to copy maps from world design folder

---
+++ dnd/copymaps.sh
@@ -0,0 +1,3 @@
+cp ../world/ardane-20mile.png __web__/static/ardane-20mile.png;
+cp ../world/allrivers-2mile.png __web__/static/allrivers-2mile.png;
+cp ../world/pearl-island-2mile.png __web__/static/pearl-island-2mile.png
