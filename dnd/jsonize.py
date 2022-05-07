2b710afab0611a57e6e367555b49905fc91aa33f
Maxwell Joslyn <maxwelljoslyn@gmail.com>
2022-05-07 09:30:04 -0700

    add Quantity and Decimal cases to custom JSON encoder

--- dnd/jsonize.py
+++ dnd/jsonize.py
@@ -1,9 +1,16 @@
 from json import JSONEncoder
+from pint import Quantity
+from decimal import Decimal
 
 
 # https://stackoverflow.com/a/8230505
-class SetEncoder(JSONEncoder):
+class MyEncoder(JSONEncoder):
     def default(self, obj):
         if isinstance(obj, set):
             return list(obj)
-        return JSONEncoder.default(self, obj)
+        elif isinstance(obj, Quantity):
+            return {"magnitude": obj.magnitude, "units": str(obj.units)}
+        elif isinstance(obj, Decimal):
+            return float(obj)
+        else:
+            return JSONEncoder.default(self, obj)
