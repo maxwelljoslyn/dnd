03443e4832d2f0e70d13277c36f22a7e74ed03a6
Maxwell Joslyn <maxwelljoslyn@gmail.com>
2022-05-16 10:33:48 -0700

    pull town setup code out into functions, encapsulating as meaningfully-named steps
    Later I will reduce/eliminate amount of work done at import time by these functions.

--- dnd/towns.py
+++ dnd/towns.py
@@ -18,6 +18,157 @@ def infrastructure(population):
     return math.floor(population / SQMI_PER_HEX.magnitude)
 
 
+def total_assigned_refs(towns):
+    assigned = defaultdict(int)
+    for d in [info["references"] for t, info in towns.items()]:
+        for k, v in d.items():
+            assigned[k] += v
+    return assigned
+
+
+def has_market(town):
+    return "markets" in original_towns[town]["references"]
+
+
+def export_quantity(amount, distance):
+    return Decimal(amount / (distance + 1))
+
+
+def far_away(towns):
+    """Add dummy town representing all the economy activity not yet represented in `towns`, at a far enough distance that exporting will not have a significant effect on existing towns' references."""
+
+    towns["Far Away"] = {"population": 5_000_000, "references": {}, "hexes to": {}}
+    # initialize distances
+    for t in towns:
+        if t != "Far Away":
+            towns[t]["hexes to"]["Far Away"] = Decimal(100)
+            towns["Far Away"]["hexes to"][t] = Decimal(100)
+    # given G global total refs to commodity X,
+    # and M total refs to X already assigned in `towns`,
+    # assign all G - M remaining refs to the "Far Away"
+    totals = total_assigned_refs(towns)
+
+    for k, info in world_references.items():
+        refs = info["references"]
+        remaining_global_refs = refs - totals[k]
+        if remaining_global_refs < 0:
+            warnings.warn(
+                f"You have assigned {abs(remaining_global_refs)} more refs to {k} than listed in world_references!"
+            )
+        refs_for_faraway = max(0, remaining_global_refs)
+        towns["Far Away"]["references"][k] = refs_for_faraway
+
+
+def initialize_towns(towns):
+    # decimalize all distances and populations
+    for town, info in towns.items():
+        info["population"] = Decimal(info["population"])
+        info["hexes to"] = {k: Decimal(v) for k, v in info["hexes to"].items()}
+
+    # fill out all-pairs shortest paths
+    for source in towns:
+        for target in towns:
+            if source == target:
+                pass
+            elif target in towns[source]["hexes to"]:
+                # distance from s to t is stipulated in original defn of towns, so reuse it
+                # NOTE assumes all distances between source and target are two-way traversable
+                towns[target]["hexes to"][source] = towns[source]["hexes to"][target]
+            else:
+                distance, path = a_star_search(towns, source, target)
+                towns[source]["hexes to"][target] = distance
+
+    far_away(towns)
+    # export step changes `towns` reference counts in-place, but export calculations require access to original counts
+    originals = copy.deepcopy(towns)
+    return towns, originals
+
+
+def export_references(towns, original_towns):
+    for source in original_towns:
+        for destination in towns:
+            if source == destination:
+                pass
+            else:
+                # given town T with N *original* references to commodity Q,
+                # and another town R at distance D,
+                # export N/(D+1) references of Q to R
+                distance = towns[source]["hexes to"][destination]
+                for thing, amount in original_towns[source]["references"].items():
+                    q = export_quantity(amount, distance)
+                    if thing in towns[destination]["references"]:
+                        towns[destination]["references"][thing] += q
+                    else:
+                        towns[destination]["references"][thing] = q
+    return towns
+
+
+def rarity_multiplier(commodity, local_refs):
+    availability_ratio = world_references[commodity]["references"] / local_refs
+    # 0.01 smoothing factor so prices don't swing wildly
+    return Decimal(1) + (Decimal(0.01) * availability_ratio)
+
+
+def determine_unit_prices(towns):
+    # TODO don't give a price for every ref, but only the raw materials:
+    # not all refs have production (e.g. markets)
+    # some refs are categories which need distribution (e.g. fruit)
+    # some refs are not raw materials and shouldn't be priced directly (e.g. woolens ... although 'hiring a woollens-maker' might be keyed off that reference, and priced according to the production thereof, no?)
+    for t, tinfo in towns.items():
+        tinfo["price"] = {}
+        for commodity, numrefs in tinfo["references"].items():
+            winfo = world_references[commodity]
+            world_refs = winfo["references"]
+            world_prod = winfo["production"]
+            if world_refs == 0 or world_prod.magnitude == 0:
+                # 'zero problem' - see below
+                continue
+            else:
+                multiplier = rarity_multiplier(commodity, numrefs)
+                price_per_ref = multiplier * cp_per_gold_ore_ref
+                tinfo["price"][commodity] = (
+                    price_per_ref / winfo["production per reference"]
+                )
+    return towns
+
+
+def infra_pop(towns):
+    mylist = sorted(towns.keys())
+    for town in mylist:
+        info = towns[town]
+        print(town)
+        pop = info["population"]
+        print(f"Pop: {pop}", f"Infra: {infrastructure(pop)}")
+
+
+coin_exchange = {
+    "gold": {
+        "weight": Decimal(0.4) * u.oz,
+        "gold content": Decimal(0.1) * u.oz,
+    },
+    "silver": {
+        "weight": Decimal(0.6) * u.oz,
+        "silver content": Decimal(0.0923815) * u.oz,
+    },
+    "copper": {
+        "weight": Decimal(0.8) * u.oz,
+        "copper content": Decimal(0.603364) * u.oz,
+    },
+}
+
+for coin, info in coin_exchange.items():
+    info["nickel content"] = info["weight"] - info[f"{coin} content"]
+
+# DETERMINING THE MONETARY VALUE OF A GOLD REFERENCE
+
+cp_per_gold_ore_ref = (
+    world_references["gold"]["production per reference"].to("oz").magnitude
+    # gold production measured in oz; official gold content of 1 gold piece = 0.1 oz
+    * (u.oz / coin_exchange["gold"]["gold content"])
+    * u.gp
+).to(u.cp)
+
+
 towns = {
     # todo add location: Hex(q, r, s)
     # todo add whether a route is by land, sea, or river
@@ -335,156 +486,12 @@ towns = {
     },
 }
 
-# decimalize all distances and populations
-for town, info in towns.items():
-    info["population"] = Decimal(info["population"])
-    info["hexes to"] = {k: Decimal(v) for k, v in info["hexes to"].items()}
+
+towns, original_towns = initialize_towns(towns)
+towns = export_references(towns, original_towns)
+towns = determine_unit_prices(towns)
 
 
-# fill out towns with all-pairs distances
-for source in towns:
-    for target in towns:
-        if source == target:
-            pass
-        elif target in towns[source]["hexes to"]:
-            # distance from s to t is stipulated in original defn of towns, so reuse it
-            # this assumes all distances between source and target are two-way traversable
-            towns[target]["hexes to"][source] = towns[source]["hexes to"][target]
-        else:
-            distance, path = a_star_search(towns, source, target)
-            towns[source]["hexes to"][target] = distance
-
-
-def far_away(towns):
-    """Add dummy town representing all the economy activity not yet represented in `towns`, at a far enough distance that exporting will not have a significant effect on existing towns' references."""
-
-    def total_assigned_refs(towns):
-        assigned = defaultdict(int)
-        for d in [info["references"] for t, info in towns.items()]:
-            for k, v in d.items():
-                assigned[k] += v
-        return assigned
-
-    towns["Far Away"] = {"population": 5_000_000, "references": {}, "hexes to": {}}
-    # initialize distances
-    for t in towns:
-        if t != "Far Away":
-            towns[t]["hexes to"]["Far Away"] = Decimal(100)
-            towns["Far Away"]["hexes to"][t] = Decimal(100)
-    # given G global total refs to commodity X,
-    # and M total refs to X already assigned in `towns`,
-    # assign all G - M remaining refs to the "Far Away"
-    for k, info in world_references.items():
-        refs = info["references"]
-        remaining_global_refs = refs - total_assigned_refs(towns)[k]
-        if remaining_global_refs < 0:
-            warnings.warn(
-                f"Maxwell has assigned {abs(remaining_global_refs)} more refs to {k} than listed in world_references!"
-            )
-        refs_for_faraway = max(0, remaining_global_refs)
-        towns["Far Away"]["references"][k] = refs_for_faraway
-
-
-far_away(towns)
-
-# DETERMINING THE MONETARY VALUE OF A GOLD REFERENCE
-
-
-coin_exchange = {
-    "gold": {
-        "weight": Decimal(0.4) * u.oz,
-        "gold content": Decimal(0.1) * u.oz,
-    },
-    "silver": {
-        "weight": Decimal(0.6) * u.oz,
-        "silver content": Decimal(0.0923815) * u.oz,
-    },
-    "copper": {
-        "weight": Decimal(0.8) * u.oz,
-        "copper content": Decimal(0.603364) * u.oz,
-    },
-}
-
-for coin, info in coin_exchange.items():
-    info["nickel content"] = info["weight"] - info[f"{coin} content"]
-
-cp_per_gold_ore_ref = (
-    world_references["gold"]["production per reference"].to("oz").magnitude
-    # gold production measured in oz; official gold content of 1 gold piece = 0.1 oz
-    * Decimal(10)
-    * u.gp
-).to(u.cp)
-
-# EXPORTING
-
-# export step will change `towns` reference counts in-place
-# but performing the calculations for those exports will require access to the original references
-original_towns = copy.deepcopy(towns)
-
-
-def has_market(town):
-    return "markets" in original_towns[town]["references"]
-
-
-def export_quantity(amount, distance):
-    return Decimal(amount / (distance + 1))
-
-
-for source in original_towns:
-    for destination in towns:
-        if source == destination:
-            pass
-        else:
-            # given town T with N *original* references to commodity Q,
-            # and another town R at distance D,
-            # export N/(D+1) references of Q to R
-            distance = towns[source]["hexes to"][destination]
-            for thing, amount in original_towns[source]["references"].items():
-                q = export_quantity(amount, distance)
-                if thing in towns[destination]["references"]:
-                    towns[destination]["references"][thing] += q
-                else:
-                    towns[destination]["references"][thing] = q
-
-
-# DETERMINING EACH TOWN'S PRICE OF 1 PRODUCTION UNIT OF COMMODITIES
-# todo don't give a price for every ref, but only the raw materials:
-# not all refs have production (e.g. markets)
-# some refs are categories which need distribution (e.g. fruit)
-# some refs are not raw materials and shouldn't be priced directly (e.g. woolens ... although 'hiring a woollens-maker' might be keyed off that reference, and priced according to the production thereof, no?)
-
-
-def rarity_multiplier(commodity, local_refs):
-    availability_ratio = world_references[commodity]["references"] / local_refs
-    # 0.01 smoothing factor so prices don't swing wildly
-    return Decimal(1) + (Decimal(0.01) * availability_ratio)
-
-
-for t, tinfo in towns.items():
-    tinfo["price"] = {}
-    for commodity, numrefs in tinfo["references"].items():
-        winfo = world_references[commodity]
-        world_refs = winfo["references"]
-        world_prod = winfo["production"]
-        if world_refs == 0 or world_prod.magnitude == 0:
-            # 'zero problem' - see below
-            continue
-        else:
-
-            multiplier = rarity_multiplier(commodity, numrefs)
-            price_per_ref = multiplier * cp_per_gold_ore_ref
-            tinfo["price"][commodity] = (
-                price_per_ref / winfo["production per reference"]
-            )
-
-
-def infra_pop():
-    mylist = sorted(towns.keys())
-    for town in mylist:
-        info = towns[town]
-        print(town)
-        pop = info["population"]
-        print(f"Pop: {pop}", f"Infra: {infrastructure(pop)}")
 
 
 def main():
