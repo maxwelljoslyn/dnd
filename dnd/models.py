f3d9ff342a1d0aa7377ded1645fb099edf573955
Maxwell Joslyn <maxwelljoslyn@gmail.com>
2022-07-23 18:42:24 -0700

    update database models

--- dnd/models.py
+++ dnd/models.py
@@ -14,11 +14,11 @@ town_model = {
         "distance": "REAL",  # i want it possible to insert a town with no connections yet specified
     },
     "material_costs": {
-        "town": "INTEGER",  # references towns.rowid
-        "name": "TEXT NOT NULL",
-        # TODO randomize material costs based on in-game year and week
-        # "year": "INTEGER NOT NULL",
-        # "week": "INTEGER NOT NULL", # TODO limit range 1-52
+        "name": "TEXT NOT NULL",  # references tradegoods.name
+        "town": "INTEGER NOT NULL",  # references towns.rowid
+        "year": "INTEGER NOT NULL",
+        "week": "INTEGER NOT NULL",  # TODO limit range 1-52
+        # TODO PRIMARY KEY (name, town, year, week)
         "price": "TEXT NOT NULL",
     },
 }
@@ -31,15 +31,16 @@ tradegood_model = {
     "prices": {
         "name": "TEXT NOT NULL",  # references tradegoods.name
         "town": "INTEGER NOT NULL",  # references towns.rowid
+        "year": "INTEGER NOT NULL",
+        "week": "INTEGER NOT NULL",  # TODO limit range 1-52
+        # TODO PRIMARY KEY (name, town, year, week)
         "price": "TEXT NOT NULL",  # a stringified pint unit
-        # TODO randomize trade good availability, based on good's final price for a given year and week
-        # "year": "INTEGER NOT NULL",
-        # "week": "INTEGER NOT NULL",  # TODO limit range 1-52
-        # "base_number_available": "INTEGER NOT NULL", # range 0 to N ... also, oh god this is updatable STATE!!!!! unless I call this BASE availability, track transactions somewhere else, another table, and compare total transactions for X against this number without updating in place
+        "base_number_available": "INTEGER NOT NULL",  # range 0 to N ... also, oh god this is updatable STATE!!!!! unless I call this BASE availability, track transactions somewhere else, another table, and compare total transactions for X against this number without updating in place
     },
 }
 
 global_model = {
+    # format Y-MM-DD / 2022-05-31
     "date": {"date": "TEXT NOT NULL"},
 }
 
